4200216761ff288e53a2a6785e14046ba9b67fc1
rajatkhanna <legalimpurity@gmail.com>
2017-09-16 11:46:45 +0530

    Review 1 done : At this line and the next line, there are unnecessary modifier's declaration.                 At this line the code could have only public interface OnInsetsCallback, without the current modifier static.                 And at the next line, the modifier public could be removed, resulting into the command void onInsetsChanged(Rect insets).

--- XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
@@ -154,7 +154,7 @@ public class DrawInsetsFrameLayout extends FrameLayout {
         }
     }
 
-    public static interface OnInsetsCallback {
-        public void onInsetsChanged(Rect insets);
+    public interface OnInsetsCallback {
+        void onInsetsChanged(Rect insets);
     }
 }
